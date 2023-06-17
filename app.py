6a49ab73da5e4301dda0079b3ff68a2538e7b263
Dwebo <100784480+dotdwebo@users.noreply.github.com>
2023-06-17 19:39:30 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+
+url = "https://api.twitter.com/2/openapi.json"
+response = requests.get(url)
+
+if response.status_code == 200:
+    json_data = response.json()
+    
+    df = pd.json_normalize(json_data)
+    # Perform further operations with the Pandas DataFrame
+    print(df.head())
+else:
+    print("Failed to download the JSON file. Status code:", response.status_code)
+
